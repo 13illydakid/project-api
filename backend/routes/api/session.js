8e0b10504485ac3bdb577b960c455eb3a0fb0971
Billy Chiu <billychiu517@gmail.com>
2023-01-24 13:53:35 -0800

    Add User input validation on user login backend endpoint

--- backend/routes/api/session.js
+++ backend/routes/api/session.js
@@ -8,28 +8,30 @@ const { User } = require('../../db/models');
 
 const router = express.Router();
 
-router.post(
-    '/',
-    async (req, res, next) => {
-        const { credential, password } = req.body;
+// Log in
+// router.post(
+//     '/',
+//     validateLogin,
+//     async (req, res, next) => {
+//       const { credential, password } = req.body;
 
-        const user = await User.login({ credential, password });
+//       const user = await User.login({ credential, password });
 
-        if (!user) {
-            const err = new Error('Login failed');
-            err.status = 401;
-            err.title = 'Login failed';
-            err.errors = ['The provided credentials were invalid.'];
-            return next(err);
-        }
+//       if (!user) {
+//         const err = new Error('Login failed');
+//         err.status = 401;
+//         err.title = 'Login failed';
+//         err.errors = ['The provided credentials were invalid.'];
+//         return next(err);
+//       }
 
-        await setTokenCookie(res, user);
+//       await setTokenCookie(res, user);
 
-        return res.json({
-            user: user
-        });
-    }
-);
+//       return res.json({
+//         user: user
+//       });
+//     }
+//   );
 
 // Log out
 router.delete(
@@ -51,6 +53,45 @@ router.get(
           user: user.toSafeObject()
         });
       } else return res.json({ user: null });
+    }
+  );
+
+  const { check } = require('express-validator');
+  const { handleValidationErrors } = require('../../utils/validation');
+
+const validateLogin = [
+    check('credential')
+      .exists({ checkFalsy: true })
+      .notEmpty()
+      .withMessage('Please provide a valid email or username.'),
+    check('password')
+      .exists({ checkFalsy: true })
+      .withMessage('Please provide a password.'),
+    handleValidationErrors
+  ];
+
+  // Log in
+router.post(
+    '/',
+    validateLogin,
+    async (req, res, next) => {
+      const { credential, password } = req.body;
+
+      const user = await User.login({ credential, password });
+
+      if (!user) {
+        const err = new Error('Login failed');
+        err.status = 401;
+        err.title = 'Login failed';
+        err.errors = ['The provided credentials were invalid.'];
+        return next(err);
+      }
+
+      await setTokenCookie(res, user);
+
+      return res.json({
+        user: user
+      });
     }
   );
 
