9cfb2d8abce81d995391a649438ae4694b2d4fec
Billy Chiu <billychiu517@gmail.com>
2023-01-26 23:31:47 -0800

    11_30_pm

--- backend/db/seeders/20230127071444-bookings-demo.js
+++ backend/db/seeders/20230127071444-bookings-demo.js
@@ -1,25 +1,44 @@
 'use strict';
 
-/** @type {import('sequelize-cli').Migration} */
+// /** @type {import('sequelize-cli').Migration} */
+
+
+const bcrypt = require("bcryptjs");
+
+let options = {};
+if (process.env.NODE_ENV === 'production') {
+  options.schema = process.env.SCHEMA;  // define your schema in options object
+}
+
+options.tableName = 'Bookings';
+
 module.exports = {
-  async up (queryInterface, Sequelize) {
-    /**
-     * Add seed commands here.
-     *
-     * Example:
-     * await queryInterface.bulkInsert('People', [{
-     *   name: 'John Doe',
-     *   isBetaMember: false
-     * }], {});
-    */
+  async up(queryInterface, Sequelize) {
+    return queryInterface.bulkInsert(
+      options,
+      [
+        {
+
+        },
+        {
+
+        },
+        {
+
+        },
+      ]
+    )
   },
 
   async down (queryInterface, Sequelize) {
-    /**
-     * Add commands to revert seed here.
-     *
-     * Example:
-     * await queryInterface.bulkDelete('People', null, {});
-     */
+    const Op = Sequelize.Op;
+    return queryInterface.bulkDelete(
+      options,
+      {
+        spotId: {
+          [Op.in]: ['']
+        },
+      }
+    )
   }
 };
