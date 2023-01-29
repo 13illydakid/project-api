dcd5ccb060d147c429345dce6d02a1aeb049210d
Billy Chiu <billychiu517@gmail.com>
2023-01-28 19:27:40 -0800

    7_25_pm

--- backend/db/seeders/20230127071043-spots-demo.js
+++ backend/db/seeders/20230127071043-spots-demo.js
@@ -1,7 +1,5 @@
 'use strict';
 
-// /** @type {import('sequelize-cli').Migration} */
-
 const bcrypt = require("bcryptjs");
 
 let options = {};
@@ -11,22 +9,51 @@ if (process.env.NODE_ENV === 'production') {
 
 options.tableName = 'Spots';
 
+/** @type {import('sequelize-cli').Migration} */
+
 module.exports = {
   async up(queryInterface, Sequelize) {
     return queryInterface.bulkInsert(
       options,
       [
         {
-
+          ownerId: 1,
+          address: '0001 Tatakae Drive',
+          city: 'Shiganshina District',
+          state: 'Wall Maria',
+          country: 'Paradis Island',
+          lat: 157.8581,
+          lng: 21.3099,
+          name: 'Residence of Eren Yeager',
+          description: '',
+          price: 845
         },
         {
-
+          ownerId: 2,
+          address: '1515 Eren Rampage Street',
+          city: 'Liberio',
+          // state: 'Destroyed',
+          state: 'Trampled',
+          country: 'Marley',
+          lat: 114.1694,
+          lng: 22.3193,
+          name: 'Internment zone',
+          description: '',
+          price: 5.99
         },
         {
-
+          ownerId: 3,
+          address: '',
+          city: 'Outer Wall Rose',
+          state: 'Wall Rose',
+          country: 'Paradis Island',
+          lat: ,
+          lng: ,
+          name: 'Utgard Castle',
+          description: '',
+          price: 17
         },
-      ]
-    )
+      ], {});
   },
 
   async down (queryInterface, Sequelize) {
@@ -41,3 +68,28 @@ module.exports = {
     )
   }
 };
+
+// {
+//   ownerId: 1,
+//   address: '0001 Tata',
+//   city: 'San Diego',
+//   state: 'California',
+//   country: 'United States',
+//   lat: ,
+//   lng: ,
+//   name: '',
+//   description: '',
+//   price:
+// },
+// {
+//   ownerId: 2,
+//   address: '',
+//   city: 'Irvine',
+//   state: 'California',
+//   country: 'United States',
+//   lat: ,
+//   lng: ,
+//   name: '',
+//   description: '',
+//   price:
+// },
