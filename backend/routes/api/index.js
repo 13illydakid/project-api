85f6a1c1bba7b5f87c6831171eb8550622de08dc
Billy Chiu <billychiu517@gmail.com>
2023-01-29 05:56:12 -0800

    5_54_am

--- backend/routes/api/index.js
+++ backend/routes/api/index.js
@@ -8,7 +8,7 @@ const reviewImagesRouter = require('./review-images.js');
 const spotsRouter = require('./spots.js');
 const spotImagesRouter = require('./spot-images.js');
 
-const { restoreUser } = require("../../utils/auth.js");
+const { restoreUser, requireAuth } = require("../../utils/auth.js");
 
 // Connect restoreUser middleware to the API router
   // If current user session is valid, set req.user to the user in the database
@@ -28,6 +28,28 @@ router.use('/spotImages', spotImagesRouter);
 router.post('/test', (req, res) => {
   res.json({ requestBody: req.body });
 });
+
+router.get('/restore-user', (req, res)=>{
+  return res.json(req.user);
+});
+
+router.get('/require-auth', requireAuth, (req, res)=>{
+  return res.json(req.user);
+});
+
+const { setTokenCookie } = require('../../utils/auth.js');
+const { User } = require('../../db/models');
+router.get('/set-token-cookie', async (_req, res) => {
+  const user = await User.findOne({
+      where: {
+        username: 'Demo-lition'
+      }
+    });
+  setTokenCookie(res, user);
+  return res.json({ user: user });
+});
+
+
 
 module.exports = router;
 
