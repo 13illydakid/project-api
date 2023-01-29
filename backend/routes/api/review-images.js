4eb8963786b542980ada5cef5a0b55276bb0cdf1
Billy Chiu <billychiu517@gmail.com>
2023-01-29 05:28:57 -0800

    5_27_am

--- backend/routes/api/review-images.js
+++ backend/routes/api/review-images.js
@@ -5,7 +5,7 @@ const { setTokenCookie, requireAuth } = require('../../utils/auth');
 const { User, Spot, SpotImage, Review, ReviewImage, Booking, sequelize } = require('../../db/models');
 
 const Sequelize = require('sequelize')
-const {Op} = require('sequelize');
+const Op = Sequelize.Op;
 
 const { handleValidationErrors } = require('../../utils/validation');
 const { check } = require('express-validator');
