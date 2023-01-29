623b5985bf3be12a6103fb62e20136d663779fd8
Billy Chiu <billychiu517@gmail.com>
2023-01-28 22:41:08 -0800

    10_39_pm

--- backend/db/seeders/20230127071428-reviewImages-demo.js
+++ backend/db/seeders/20230127071428-reviewImages-demo.js
@@ -18,27 +18,27 @@ module.exports = {
       options,
       [
         {
-
+          reviewId: 1,
+          url: 'https://archello.s3.eu-central-1.amazonaws.com/images/2018/03/31/American-style-house-interior-design-in-Dammam-2.1522524114.1883.jpg'
         },
         {
-
+          reviewId: 2,
+          url: 'https://i.pinimg.com/originals/9a/28/6f/9a286f1b388c258087a1140bbdbe499a.jpg'
         },
         {
-
+          reviewId: 3,
+          url: 'https://livinginatiny.com/wp-content/uploads/2021/02/Tiny-house-with-amazing-interior-design_3.jpeg'
         },
-      ]
-    )
+      ], {});
   },
 
-  async down (queryInterface, Sequelize) {
+  async down(queryInterface, Sequelize) {
     const Op = Sequelize.Op;
     return queryInterface.bulkDelete(
-      options,
-      {
-        reviewId: {
-          [Op.in]: ['']
-        },
-      }
-    )
+      options, {
+      reviewId: {
+        [Op.in]: [1, 2, 3]
+      },
+    });
   }
 };
