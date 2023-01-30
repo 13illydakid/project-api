6bff0797b89527e6e317a9ea2dd484dfc6c39b5c
Billy Chiu <billychiu517@gmail.com>
2023-01-29 23:56:16 -0800

    11_54_pm

--- backend/routes/api/reviews.js
+++ backend/routes/api/reviews.js
@@ -13,7 +13,7 @@ const { check } = require('express-validator');
 // const { requireAuth } = require('../../utils/auth');
 const review = require('../../db/models/review');
 
-const validateSignup = [
+const validateReviews = [
     check('review')
         .exists({ checkFalsy: true })
         .withMessage('Review text is required'),
@@ -74,3 +74,116 @@ router.get('/current', requireAuth, async (req, res, next) => {
         Reviews: reviewArray
     });
 });
+
+
+//Add an image to a Review based on the Review's id
+router.post("/:reviewId/images", requireAuth, async (req, res) => {
+    const reviewId = req.params.reviewId;
+    const url = req.body.url;
+    const yesUser = req.user;
+
+    const review = await Review.findByPk(reviewId);
+    const reviewImages = await ReviewImage.findAll({
+        where: {
+            reviewId: reviewId
+        }
+    });
+
+    if (!review) {
+        res.statusCode = 404;
+        return res.json({
+            message: "Review couldn't be found",
+            statusCode: 404
+        })
+    }
+    if (yesUser.id !== review.userId) {
+        res.statusCode = 403;
+        return res.json({
+            message: "Forbidden",
+            statusCode: 403
+        });
+    }
+
+    if(reviewImages.length > 10){
+        res.status(403);
+        return res.json({
+            message: "Maximum number of images for this resource was reached",
+            statusCode: 403
+        });
+    }
+
+    const newImage = await ReviewImage.create({
+        reviewId,
+        url
+    });
+    res.status(200);
+    res.json({
+        id: newImage.id,
+        url: newImage.url
+    });
+});
+
+// Editing a review
+router.put('/:reviewId', [requireAuth, validateReviews], async (req, res) => {
+    const reviewId = req.params.reviewId;
+    const { star, review } = req.body;
+
+    const selectReview = await Review.findByPk(reviewId);
+
+    const currentUser = req.user;
+
+    if (!selectReview) {
+        res.status(404);
+        return res.json({
+            message:  "Review couldn't be found",
+            statusCode: 404
+        });
+    }
+
+    if (currentUser.id !== selectReview.userId) {
+        res.status(403);
+            return res.json({
+                message: "Forbidden",
+                statusCode: 403
+            });
+    }
+
+    await selectReview.update({
+        star,
+        review
+    });
+
+    return res.json(selectReview);
+});
+
+//Delete a Review
+router.delete('/:reviewId', requireAuth, async (req, res) => {
+    const selectUser = req.user;
+    const reviewId = req.params.reviewId;
+
+    const selectReview = await Review.findByPk(reviewId);
+
+    if (!selectReview) {
+        res.status(404);
+        return res.json({
+            message: "Review couldn't be found",
+            statusCode: 404
+        });
+    }
+    if (selectUser.id !== selectReview.userId) {
+        res.status(403);
+            return res.json({
+                message: "Forbidden",
+                statusCode: 403
+            });
+    }
+    await selectReview.destroy();
+    res.status(200);
+    return res.json({
+        message: "Successfully deleted",
+        statusCode: 200
+    });
+});
+
+
+module.exports = router;
