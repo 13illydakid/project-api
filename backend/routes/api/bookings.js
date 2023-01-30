273d17259d30b65a26898a9208c8c176be1260d2
Billy Chiu <billychiu517@gmail.com>
2023-01-30 15:01:20 -0800

    2-59-pm

--- backend/routes/api/bookings.js
+++ backend/routes/api/bookings.js
@@ -2,191 +2,193 @@ const express = require('express');
 const router = express.Router();
 
 const { setTokenCookie, requireAuth } = require('../../utils/auth');
-const { User, Spot, SpotImage, Review, ReviewImage, Booking } = require('../../db/models');
-const sequelize = require('sequelize');
-const { handleValidationErrors } = require('../../utils/validation');
-const { validateBooking } = require('../../utils/validation');
+const { User, Spot, Review, SpotImage, ReviewImage, Booking } = require('../../db/models');
+
 const { check } = require('express-validator');
-const { convertDate } = require('../../utils/error-handlers')
-const { ifBookingExists } = require('../../utils/error-handlers')
-//Get all of the Current User's Booking
-router.get('/current', requireAuth, async (req, res, next) => {
-    const currentUserId = req.user.id;
-    const bookings = await Booking.findAll({
-        where: { userId: currentUserId },
-        include: [
-            {
-                model: Spot,
-                include: [{
-                    model: SpotImage
-                }],
-                attributes: {
-                    excludes: ['description', 'createdAt', 'updatedAt']
-                },
+const { handleValidationErrors, validateBooking } = require('../../utils/validation');
+const sequelize = require('sequelize');
+
+const { convertDate, ifBookingExists } = require('../../utils/error-handlers')
+
+
+// Get bookings for current user
+router.get("/current", requireAuth, async (req, res, next) => {
+    const user = req.user;
+
+    const bookings = await user.getBookings({
+        include: [{
+            model: Spot,
+            attributes: ['id', 'ownerId', 'address', 'city', 'state', 'country', 'lat', 'lng', 'name', 'price'],
+            include: [{
+                model: SpotImage,
+                attributes: ['url']
+            }]
+        }]
+    });
+
+    if (!bookings.length > 0) {
+        return res.json({
+            message: "No bookings for current spot"
+        })
+    }
+
+    const bookingsArr = [];
+    bookings.forEach(booking => {
+        booking = booking.toJSON();
+        if (booking.Spot.SpotImages.length > 0) {
+            for (let i = 0; i < booking.Spot.SpotImages.length; i++) {
+                if (booking.Spot.SpotImages[i].preview === true) {
+                    booking.Spot.previewImage = booking.Spot.SpotImages[i].url;
+                }
             }
-        ],
-    });
-    const userAllBookings = [];
-    const arrayBookings = [];
-    for (let booking of bookings) {
-        let bookingStr = booking.toJSON();
-        userAllBookings.push(bookingStr);
+        }
+
+        if (!booking.Spot.previewImage) {
+            booking.Spot.previewImage = "No preview image available";
+        }
+
+        delete booking.Spot.SpotImages;
+        const eachBooking = {
+            id: booking.id,
+            spotId: booking.spotId,
+            Spot: booking.Spot,
+            userId: booking.userId,
+            startDate: booking.startDate,
+            endDate: booking.endDate,
+            createdAt: booking.createdAt,
+            updatedAt: booking.updatedAt
+        }
+        bookingsArr.push(eachBooking);
+    })
+
+    res.json({
+        Bookings: bookingsArr
+    })
+})
+
+// Edit a Booking
+router.put('/:bookingId', requireAuth, ifBookingExists, validateBooking, async (req, res, next) => {
+    const { bookingId } = req.params;
+    const user = req.user;
+    let { startDate, endDate } = req.body;
+    startDate = convertDate(startDate);
+    endDate = convertDate(endDate);
+
+    let bookingToEdit = await Booking.findByPk(bookingId);
+
+    let err = {};
+    if (startDate <= new Date()) {
+        err.title = "Can't start a booking in the past";
+        err.status = 403;
+        err.message = "Start date cannot be before today"
+        return next(err)
     }
-    for (let booking of userAllBookings) {
-        // const previewImage = booking.Spot.SpotImages;
-        for (let image of booking.Spot.SpotImages) {
-            if (image.preview) {
-                booking.Spot.previewImage = image.url;
-            } else {
-                booking.Spot.previewImage = 'Preview image not available';
+
+    bookingStartDate = convertDate(bookingToEdit.startDate);
+    bookingEndDate = convertDate(bookingToEdit.endDate);
+    const spotId = bookingToEdit.spotId;
+
+    if (bookingEndDate < new Date()) {
+        err.title = "Can't edit a booking that's past the end date";
+        err.status = 403;
+        err.message = "Past bookings can't be modified";
+        return next(err);
+    };
+
+    if (endDate <= startDate) {
+        err.title = "Validation error";
+        err.statusCode = 400;
+        err.message = "endDate cannot be on or before startDate";
+        return next(err);
+    };
+
+    /// If booking belongs to current user
+    if (user.id !== bookingToEdit.userId) {
+        err.title = "Authorization error";
+        err.status = 403;
+        err.message = "Booking doesn't belong to current user";
+        return next(err);
+    }
+
+    const spot = await Spot.findByPk(spotId);
+
+    const bookings = await spot.getBookings();
+
+    bookings.forEach(booking => {
+        if (booking.id !== bookingToEdit.id) {
+
+
+            booking = booking.toJSON();
+            err.title = "Booking Conflict";
+            err.status = 403;
+            err.message = "Sorry, this spot is already booked for the specified dates";
+
+            bookedStartDate = convertDate(booking.startDate);
+            bookedEndDate = convertDate(booking.endDate);
+
+            if ((bookedStartDate <= startDate) && bookedEndDate >= startDate) {
+                err.errors = [
+                    { startDate: "Start date conflicts with an existing booking" }
+                ]
+                return next(err);
+            } else if (((bookedStartDate <= endDate) && (endDate <= bookedEndDate))) {
+                err.errors = [
+                    { endDate: "End date conflicts with an existing booking" }
+                ]
+                return next(err);
+            } else if ((bookedStartDate >= startDate) && (bookedEndDate <= endDate)) {
+                err.errors = [
+                    { startDate: "Start date conflicts with an existing booking" },
+                    { endDate: "End date conflicts with an existing booking" }
+                ]
+                return next(err);
             }
         }
-        arrayBookings.push(booking);
-    }
-    return res.json({ Bookings: arrayBookings });
-});
-//edit a booking
-router.put('/:bookingId', requireAuth, async (req, res) => {
-    const { startDate, endDate } = req.body;
-    const starting = new Date(startDate);
-    const ending = new Date(endDate);
 
+    });
+
+    if (!err.errors) {
+        bookingToEdit.startDate = startDate;
+        bookingToEdit.endDate = endDate;
+        bookingToEdit.save();
+        res.json(bookingToEdit)
+    }
+})
+
+// Delete a Booking
+router.delete('/:bookingId', requireAuth, ifBookingExists, async (req, res, next) => {
     const { bookingId } = req.params;
-    const { user } = req;
+    const user = req.user;
 
-    const selectBooking = await Booking.findOne({
-        where: {
-            id: bookingId
-        }
-    });
-    //Error if Booking is not found
-    if (!selectBooking) {
-        res.status(404);
-        return res.json({
-            message: "Booking couldn't be found",
-            statusCode: 404,
-        });
-    }
-    //User does not have the correct authorization
-    if (selectBooking.userId !== user.Id) {
-        res.status(403);
-        return res.json({
-            message: "Forbidden",
-            statusCode: 403
-        });
+    let booking = await Booking.findByPk(bookingId);
+
+    let spot = await booking.getSpot();
+
+    let err = {};
+    // If booking doesn't belong to user or spot owner
+    if (user.id !== booking.userId && user.id !== spot.ownerId) {
+        err.title = "Authorization error";
+        err.status = 403;
+        err.message = "Booking doesn't belong to current user";
+        return next(err);
     }
 
-    // if startDate is after endDate
-    if (starting.getTime() >= ending.getTime()) {
-        res.status(400);
-        return res.json({
-            message: 'Validation error',
-            statusCode: 400,
-            errors: [
-                'endDate cannot come before startDate'
-            ]
-        });
-    }
-    //Can't edit a booking that's past the end date
-    const today = new Date();
-    if (today > ending.getTime()) {
-        res.statusCode(403);
-        return res.json({
-            message: "Forbidden",
-            statusCode: 403,
-            errors: [
-                "Past bookings can't be modified"
-            ]
-        });
-    }
-    //Booking conflict
-    const selectBookingSpotId = selectBooking.spotId;
-    const reservations = await Booking.findAll({
-        where: {
-            selectBookingSpotId,
-        }
-    });
-    for (let reserved of reservations) {
-        if ((starting.getTime() === reserved.startDate.getTime()) || ((starting.getTime() > reserved.startDate.getTime()) && (reserved.endDate.getTime() > starting.getTime()))) {
-            res.status(403);
-            return res.json({
-                message: "Sorry, this spotis already booked for the specified dates",
-                statusCode: 403,
-                errors: [
-                    'Start date conflicts with an existing booking'
-                ]
-            });
-        }
-        if ((ending.getTime() === reserved.endDate.getTime()) || ((ending.getTime() > reserved.startDate.getTime()) && (reserved.endDate.getTime() > ending.getTime()))) {
-            res.status(403);
-            return res.json({
-                message: "Sorry, this spotis already booked for the specified dates",
-                statusCode: 403,
-                errors: [
-                    'End date conflicts with an existing booking'
-                ]
-            });
-        }
-    }
-    function confirmChange() {
-        selectBooking.update({
-            startDate,
-            endDate
-        });
-        res.json(selectBooking);
-    }
-    confirmChange();
-});
+    const startDate = convertDate(booking.startDate);
 
-//delete a Booking
-router.delete('/:bookingId', requireAuth, async(req, res, next)=>{
-    const { bookingId } = req.params;
-    const { user } = req;
-    const selectBooking = await Booking.findOne({
-        where: {
-            id: bookingId
-        }
-    });
-    if (!selectBooking) {
-        res.status(404);
-        return res.json({
-            message: "Booking couldn't be found",
-            statusCode: 404,
-        });
-    }
-    const today = new Date();
-    const starting = new Date(selectBooking.startDate);
-    if(starting.getTime() <= today.getTime()){
-        res.status(403);
-        return res.json({
-            message: "Forbidden",
-            statusCode: 403,
-            errors: [
-                "Past bookings can't be modified"
-            ]
-        });
-    }
-    const selectSpot = await Spot.findOne({
-        where: {
-            id: selectBooking.spotId
-        }
-    });
-    if((user.id === selectBooking.userId) && (user.id === selectSpot.ownerId)){
-        await selectBooking.destroy();
-        res.status(200);
-        return res.json({
-            message: 'Successfully deleted',
-            statusCode: 200
-        });
-    } else{
-        res.status(403);
-        return res.json({
-            message: 'Forbidden',
-            statusCode: 403
-        });
-    }
-});
+    if (startDate <= new Date()) {
+        err.title = "Bookings that have been started or completed can't be deleted";
+        err.status = 403;
+        err.message = "Bookings that have been started or completed can't be deleted";
+        return next(err)
+    };
+
+    booking.destroy();
+    return res.json({
+        "message": "Successfully deleted",
+        "statusCode": 200
+    })
+})
+
+
 
 module.exports = router;
-
-//nothing
