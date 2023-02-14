e16826c306783d13a8a450d20a323900dfa66d26
Billy Chiu <billychiu517@gmail.com>
2023-02-13 20:22:33 -0800

    8_20_pm

--- frontend/src/store/spots.js
+++ frontend/src/store/spots.js
@@ -41,19 +41,99 @@ export const RemoveSpot = (spotId) => {
         spotId
     }
 }
-export const AddImage = (img) => {
+export const AddImage = (spotId, url, preview) => {
     return {
         type: ADD_IMAGE,
-        img
+        spotId,
+        url,
+        preview
     }
 }
+// export const AddImage = (img) => {
+//     return {
+//         type: ADD_IMAGE,
+//         img
+//     }
+// }
 
 // thunks
 export const getSingleSpotThunk = () => async (dispatch) => {
-    const res = await csrfFetch('/api/spots');
+    const res = await csrfFetch(`/api/spots/${spotId}`);
     if(res.ok){
         const spot = await res.json();
         dispatch(GetSingleSpot(spot));
         return spot;
+    }
+}
+export const getAllSpotsThunk = () => async (dispatch) => {
+    const res = await csrfFetch('/api/spots');
+    if(res.ok){
+        const spots = await res.json();
+        dispatch(GetAllSpots(spots));
+        return spots;
+    }
+}
+export const createSpotThunk = (spot) => async (dispatch) => {
+    const res = await csrfFetch('/api/spots', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }, body: JSON.stringify(spot)
+    });
+    if(res.ok){
+        const spot = await res.json();
+        dispatch(CreateSpot(spot));
+        return spot;
+    }
+}
+export const editSpotThunk = (spotId, spot) => async (dispatch) => {
+    const res = await csrfFetch(`/api/spots/${spotId}`, {
+        method: 'PUT',
+        headers: {
+            'Content-Type': 'applicaiton/json'
+        }, body: JSON.stringify(spot)
+    });
+    if(res.ok){
+        const spot = await res.json();
+        dispatch(EditSpot(spotId, spot));
+        return spot;
+    }
+}
+export const removeSpotThunk = (spotId) => async (dispatch) => {
+    const res = await csrfFetch(`/api/spots/${spotId}`, {
+        method: 'DELETE'
+    });
+    if(res.ok){
+        dispatch(RemoveSpot(spotId));
+    }
+}
+export const addImageThunk = (spotId, url, preview) => async (dispatch) => {
+    const res = await csrfFetch(`/api/spots/${spotId}/images`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        }, body: JSON.stringify({ url, preview })
+    });
+    if(res.ok){
+        const { spotId, url, preview } = await res.json();
+        dispatch(AddImage(spotId, url, preview));
+        return spotId;
+    }
+}
+// end
+
+const initialState = {
+    singleSpot: {},
+    allSpots: {}
+}
+
+// reducer
+function spotsReducer ( state = initialState, action ) {
+    const newState;
+    switch(action.type){
+        case GET_SINGLE_SPOT: {
+            newState = { ...state }
+            newState.spots = 
+        }
     }
 }
