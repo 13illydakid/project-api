d875b87233dbe3bc03db989d38f2d3b336663cb1
Billy Chiu <billychiu517@gmail.com>
2023-02-15 12:54:34 -0800

    12_54_pm

--- frontend/src/components/Spots/RemoveSpot/RemoveSpot.js
+++ frontend/src/components/Spots/RemoveSpot/RemoveSpot.js
@@ -5,8 +5,44 @@ import { removeSpotThunk } from '../../..store/spots';
 import { useModal } from '../../../context/Modal';
 import './RemoveSpot.css';
 
-export default function RemoveSpot(spot){
+export default function RemoveSpot(spot) {
     const dispatch = useDispatch();
     const history = useHistory();
-    const 
+    const { noModal } = useModal();
+    thisSpot = spot.spot;
+
+    const [error, setError] = useState([]);
+    const revert = (i) => {
+        i.preventDefault();
+        noModal();
+    }
+    const deleteNow = async (i) => {
+        i.preventDefault();
+        const deletion = await dispatch(
+            removeSpotThunk(thisSpot.id)
+        ).catch(async (res) => {
+            const datas = await res.json();
+            if (datas) {
+                if (datas.error) {
+                    setError(datas.error);
+                }
+            }
+        });
+        noModal();
+        history.pushState('/');
+    }
+
+    return (
+        <div className="form-div">
+            <h1 className="title">Confirm deletion of this Spot</h1>
+            <ul className="error">
+                {error.map((err, j) => {
+                    <li key={j}>{err}</li>
+                })}
+            </ul>
+            <form className="form">
+                <button type="submit" className="submit-button" id="removeSpot-button" onClick={deleteNow}>Delete Now</button>
+            </form>
+        </div>
+    )
 }
