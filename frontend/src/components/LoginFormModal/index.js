96791ebf313fe75f5c575115f0c3be82c9654768
Billy Chiu <billychiu517@gmail.com>
2023-02-20 07:31:47 -0800

    7-31-am

--- frontend/src/components/LoginFormModal/index.js
+++ frontend/src/components/LoginFormModal/index.js
@@ -1,14 +1,13 @@
-// frontend/src/components/LoginFormModal/index.js
-import React, { useState } from "react";
-import * as sessionActions from "../../store/session";
-import { useDispatch } from "react-redux";
-import { useModal } from "../../context/Modal";
-import "./LoginForm.css";
+import React, { useState } from 'react';
+import * as sessionActions from '../../store/session';
+import { useDispatch } from 'react-redux';
+import { useModal } from '../../context/Modal';
+import './LoginForm.css';
 
 function LoginFormModal() {
   const dispatch = useDispatch();
-  const [credential, setCredential] = useState("");
-  const [password, setPassword] = useState("");
+  const [credential, setCredential] = useState('');
+  const [password, setPassword] = useState('');
   const [errors, setErrors] = useState([]);
   const { closeModal } = useModal();
 
@@ -17,44 +16,52 @@ function LoginFormModal() {
     setErrors([]);
     return dispatch(sessionActions.login({ credential, password }))
       .then(closeModal)
-      .catch(
-        async (res) => {
-          const data = await res.json();
-          if (data && data.errors) setErrors(data.errors);
-        }
-      );
+      .catch(async (res) => {
+        const data = await res.json();
+        if (data && data.errors) setErrors(data.errors);
+      });
   };
 
   return (
-    <>
-      <h1>Log In</h1>
+    <div className="login-container">
       <form onSubmit={handleSubmit}>
+        <div className="modalHead">Welcome !</div>
         <ul>
           {errors.map((error, idx) => (
-            <li key={idx}>{error}</li>
+            <div style={{ color: 'red' }} key={idx}>
+              {error}
+            </div>
           ))}
         </ul>
-        <label>
-          Username or Email
-          <input
-            type="text"
-            value={credential}
-            onChange={(e) => setCredential(e.target.value)}
-            required
-          />
-        </label>
-        <label>
-          Password
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-            required
-          />
-        </label>
-        <button type="submit">Log In</button>
+        <div className="form-wrapper">
+          <label className="login-text">
+            Username or Email
+            <div>
+              <input
+                type="text"
+                value={credential}
+                onChange={(e) => setCredential(e.target.value)}
+                required
+              />
+            </div>
+          </label>
+          <label className="login-text">
+            Password
+            <div>
+              <input
+                type="password"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+                required
+              />
+            </div>
+          </label>
+        </div>
+        <button className="modal-submit-button" type="submit">
+          Log In
+        </button>
       </form>
-    </>
+    </div>
   );
 }
 
--- frontend/src/components/PageNotFound/index.js
+++ frontend/src/components/PageNotFound/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { useHistory } from 'react-router-dom';
-import './Pagenotfound.css';
+import './PageNotFound.css';
 
 export default function PageNotFound() {
   const history = useHistory();
