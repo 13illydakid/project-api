9ed50103bc65ade62537d151e942b44df3ac824a
Billy Chiu <billychiu517@gmail.com>
2023-02-22 02:03:20 -0800

    2-02

--- frontend/src/store/reviews.js
+++ frontend/src/store/reviews.js
@@ -1,184 +1,157 @@
 import { csrfFetch } from "./csrf";
 
 // actions
-const GET_SPOT_REVIEWS = 'spot/GET_SPOT_REVIEWS';
-const GET_USER_REVIEWS = 'spot/GET_USER_REVIEWS';
-const ADD_REVIEW = 'spot/ADD_REVIEW';
-const EDIT_REVIEW = 'spot/EDIT_REVIEW';
-const REMOVE_REVIEW = 'spot/REMOVE_REVIEW';
-const RESET_REVIEWS = 'reviews/RESET_REVIEWS';
+// const GET_SPOT_REVIEWS = 'reviews/GET_SPOT_REVIEWS';
+// const GET_USER_REVIEWS = 'reviews/GET_USER_REVIEWS';
+// const EDIT_REVIEW = 'reviews/EDIT_REVIEW';
+// const RESET_REVIEWS = 'reviews/RESET_REVIEWS';
+const GET_REVIEWS = 'reviews/GET_REVIEWS';
+const ADD_REVIEW = 'reviews/ADD_REVIEW';
+const REMOVE_REVIEW = 'reviews/REMOVE_REVIEW';
 
 // action creators
-export const getSpotReviews = (reviews) => {
+
+// const getSpotReviews = (reviews) => {
+//     return {
+//         type: GET_SPOT_REVIEWS,
+//         // spotId,
+//         reviews
+//     }
+// }
+// const getUserReviews = (reviews) => {
+//     return {
+//         type: GET_USER_REVIEWS,
+//         // userId,
+//         reviews
+//     }
+// }
+const getReviews = (payload) => {
     return {
-        type: GET_SPOT_REVIEWS,
+        type: GET_REVIEWS,
         // spotId,
-        reviews
+        payload
     }
 }
-export const getUserReviews = (reviews) => {
-    return {
-        type: GET_USER_REVIEWS,
-        // userId,
-        reviews
-    }
-}
-export const addReview = (review) => {
+
+const addReview = (payload) => {
     return {
         type: ADD_REVIEW,
         // spotId,
-        review
+        payload
     }
 }
-export const editReview = (review) => {
-    return {
-        type: EDIT_REVIEW,
-        // reviewId,
-        review
-    }
-}
-export const removeReview = (reviewId) => {
+const removeReview = (reviewId) => {
     return {
         type: REMOVE_REVIEW,
         reviewId
     }
 }
-export const resetReviews = () => {
-    return {
-      type: RESET_REVIEWS,
-    };
-  }
+// const editReview = (review) => {
+//     return {
+//         type: EDIT_REVIEW,
+//         // reviewId,
+//         review
+//     }
+// }
+// const resetReviews = () => {
+//     return {
+//       type: RESET_REVIEWS,
+//     };
+//   }
 
 // thunks
+
+// export const getUserReviewsThunk = () => async (dispatch) => {
+//     const res = await csrfFetch('/api/reviews/current');
+//     if(res.ok){
+//         const reviews = await res.json();
+//         dispatch(getUserReviews(reviews));
+//         return reviews;
+//     }
+// }
 export const getSpotReviewsThunk = (spotId) => async (dispatch) => {
     const res = await csrfFetch(`/api/spots/${spotId}/reviews`);
-    if(res.ok){
-        const reviews = await res.json();
-        dispatch(getSpotReviews(reviews));
-        return reviews;
+    if (res.ok) {
+        const payload = await res.json();
+        dispatch(getReviews(payload));
+        // return payload;
     }
 }
-export const getUserReviewsThunk = () => async (dispatch) => {
-    const res = await csrfFetch('/api/reviews/current');
-    if(res.ok){
-        const reviews = await res.json();
-        dispatch(getUserReviews(reviews));
-        return reviews;
-    }
-}
-export const addReviewThunk = (newReview, spotId, user) => async (dispatch) => {
-        const res = await csrfFetch(`/api/spots/${spotId}/reviews`, {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json' },
-          body: JSON.stringify(newReview),
-        });
 
-        if (!res.ok) {
-          let error;
-          if (res.status === 403) {
-            error = await res.json();
-            return error.message;
-
-          }
-          console.log(error)
-        }
-
-        if (res.ok) {
-          const newReview = await res.json();
-          const userInfo = {};
-          userInfo.id = user.id;
-          userInfo.firstName = user.firstName;
-          userInfo.lastName = user.lastName;
-          newReview.User = userInfo;
-          newReview.ReviewImages = [];
-
-          dispatch(addReview(newReview));
-
-          return newReview;
-        } else {
-          const result = await res.json();
-          return result;
-        }
-}
-export const editReviewThunk = (reviewId, review) => async (dispatch) => {
-    const res = await csrfFetch(`/api/reviews/${reviewId}`, {
-        method: 'PUT',
+export const addReviewThunk = (data, spotId) => async (dispatch) => {
+    const res = await csrfFetch(`/api/spots/${data.spotId}/reviews`, {
+        method: 'POST',
         headers: {
             'Content-Type': 'application/json'
-        }, body: JSON.stringify(review)
+        }, body: JSON.stringify(data),
     });
+
     if(res.ok){
-        const newReview = await res.json();
-        dispatch(editReview(review));
-        return newReview;
+        const payload = await res.json();
+        dispatch(addReview(payload));
+        // return payload;
     }
 }
 export const removeReviewThunk = (reviewId) => async (dispatch) => {
     const res = await csrfFetch(`/api/reviews/${reviewId}`, {
         method: 'DELETE'
     });
-    if(res.ok){
-        const formerReview = await res.json();
+    if (res.ok) {
         dispatch(removeReview(reviewId));
-        return formerReview;
     }
 }
+// export const editReviewThunk = (reviewId, review) => async (dispatch) => {
+//     const res = await csrfFetch(`/api/reviews/${reviewId}`, {
+//         method: 'PUT',
+//         headers: {
+//             'Content-Type': 'application/json'
+//         }, body: JSON.stringify(review)
+//     });
+//     if (res.ok) {
+//         const newReview = await res.json();
+//         dispatch(editReview(review));
+//         return newReview;
+//     }
+// }
 
 // reducer
 const initialState = {
-    user: {},
-    spot: {}
+    spotReviews: {}
 }
 
-const reviewsReducer = (state=initialState, action) => {
+const reviewsReducer = (state = initialState, action) => {
     let newState;
-    switch(action.type){
-        case GET_SPOT_REVIEWS: {
-            newState = { ...state };
-            const normalizedReview = {};
-            action.reviews.forEach((review) => {
-              normalizedReview[review.id] = review;
-            });
-            newState.spot = normalizedReview;
-            newState.user = {};
-            return newState;
-        }
-        case GET_USER_REVIEWS: {
+    switch (action.type) {
+        case GET_REVIEWS: {
             newState = { ...state };
             const normalizedUserReviews = {};
-            action.reviews.forEach(
-              (review) => (normalizedUserReviews[review.id] = review)
+            action.payload.Reviews.forEach(
+                (review) => (normalizedUserReviews[review.id] = review)
             );
-            newState.user = normalizedUserReviews;
-            newState.spot = {};
+            newState.spotReviews = normalizedUserReviews;
             return newState;
         }
         case ADD_REVIEW: {
             newState = { ...state };
-            newState.user = { ...state.user };
-            newState.spot = { ...state.spot, [action.review.id]: action.review };
+            newState.spotReviews[action.payload.id] = action.payload
             return newState;
         }
-        case EDIT_REVIEW: {
-            return { ...state, spot: { ...state.spot, [action.review.id]: action.review } }
-        }
+        // case EDIT_REVIEW: {
+        //     return { ...state, spot: { ...state.spot, [action.review.id]: action.review } }
+        // }
         case REMOVE_REVIEW: {
-            newState = { ...state };
-            newState.spot = { ...state.spot };
-            newState.user = { ...state.user };
-            delete newState.spot[action.reviewId];
-            delete newState.user[action.reviewId];
+            newState = { ...state.spotReviews };
+            delete newState[action.reviewId];
             return newState;
         }
-        case RESET_REVIEWS: {
-            newState = { ...state };
-            newState.user = {};
-            newState.spot = {};
-            return newState;
-        }
+        // case RESET_REVIEWS: {
+        //     newState = { ...state };
+        //     newState.user = {};
+        //     newState.spot = {};
+        //     return newState;
+        // }
         default: {
-            newState = { ...state }
-            return newState;
+            return state;
         }
     }
 }
